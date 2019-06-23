aeda2af901e14075112983a878904361ac7258a0
Dileep Rajput <dileep_rajput@yahoo.com>
2019-06-23 14:58:47 -0400

    Add doc strings to every helper method

--- cowsay.py
+++ cowsay.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
 
+#padding around speech bubble text
 borderpadding = 2
+
+#cow ascii art
 cow = [
     "          \\  ^__^",
     "           \\ (oo)\\________",
@@ -16,10 +19,20 @@ def main():
     drawAnimal()
 
 def drawAnimal():
+    """Draws a cow in the terminal with ascii art"""
     for line in cow:
         print(line)
 
 def drawTextBox(lines):
+    """draws a textbox using the ascii art into the terminal. The textbox
+       will contain the lines passed into this function.
+
+    Parameters:
+        lines (array of strings) : the text that will be inside the textbox
+
+    Returns:
+        None
+    """
     maxlinelength = getMaxLineLength(lines) + borderpadding
     horizontal_border = " " + ('-' * maxlinelength)
     print(horizontal_border)
@@ -35,12 +48,30 @@ def drawTextBox(lines):
     print(horizontal_border)
 
 def getLines():
+    """return lines in stdin as an array of strings with whitespace stripped.
+
+    Parameters: None.
+
+    Returns:
+        (array of strings) : lines of text sent to stdin
+    """
+
     lines = []
     for line in sys.stdin:
         lines.append(line.strip())
     return lines
 
 def getMaxLineLength(lines):
+    """Return the length of the longest line of text in the array of lines
+
+    Parameters:
+        lines (array of strings) : the array that contains the lines that will
+                                    be searched.
+
+    Returns:
+        (int) : length of the longest string in the array
+    """
+
     maxLength = 0
     for line in lines:
         length = len(line)
@@ -49,6 +80,17 @@ def getMaxLineLength(lines):
     return maxLength
 
 def padLine(line, maxlinelength):
+    """Add whitespace to the end of the given line so it's length equals maxlinelength
+
+    Parameters:
+        line (string) : the line that will have whitespace added to it.
+        maxlinelnegth (int) : the target length of the line after it has
+                              been padded.
+
+    Returns:
+        (string) : the line parameter padded with whitespace at the end.
+    """
+
     paddingLength = maxlinelength - len(line) - borderpadding
     padding = ""
     if paddingLength > 0:
